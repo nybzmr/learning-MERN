33c1f520047f4aa9373a489dd63ad8bc08027552
nybzmr <nybzmr02@gmail.com>
2024-07-03 09:36:40 +0530

    86

---
+++ 86/mymodule.js
@@ -0,0 +1,11 @@
+export const a = 1;
+export const b = 2
+export const c = 3
+export const d = 4
+export const e = 5
+export const f = 6 //named export
+let obj;
+export default obj = {
+    x:5,
+    y:7
+} // default export
--- 86/server.js
+++ 86/server.js
@@ -1,14 +1,30 @@
-const { createServer } = require('node:http');
+// import http from "http"
+// import {a} from "./mymodule.js"
+// import object from "./mymodule.js"
+// console.log(object)
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World<h1> <p>How are you<p>');
-});
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// // whenever we write in commonjs the Module code gets wrapped in the following function
+// (function(exports, require, module, __filename, __dirname){
+//   //Module code actually lives here
+// })
+
+
+
+
+
+
+// const { createServer } = require('node:http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// const server = createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/html');
+//   res.end('<h1>Hello World<h1> <p>How are you<p>');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
